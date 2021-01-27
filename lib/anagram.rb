f26e9b349370a229bb4bc00ed8731b95d1563582
Justin Movitz <movitzj1@montclair.edu>
2021-01-27 15:57:00 -0500

    commit tree db739202961039976b4db11178643e765b5138a5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+    # To determine if they are anagrams, try determining if they are composed of the same letters. 
+    # Remember that you can split a word into an array of letters using some_word.split(""). 
+    # You can compare two arrays using the ==.
+    # Two arrays are equal if they contain the same elements, in the same order. 
+    # Remember that you can .sort an array's elements.
+    def match(possible_anagrams)
+        possible_anagrams.select do |anagram|
+            anagram.chars.sort == word.chars.sort
+        end
+    end
+end
+        
